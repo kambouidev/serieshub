d5419f3d565f2ffa79a74d364a73155d00a64ac7
kambouidev <mohamed@squaads.com>
2023-11-18 23:36:36 +0000

    refactor: some fixes

--- src/components/SHSearchBar/SHSearchBar.component.tsx
+++ src/components/SHSearchBar/SHSearchBar.component.tsx
@@ -42,7 +42,7 @@ const SHSearchBar = () => {
 
     return (
         <div className="searchContainer  sm:p-4 md:p-6 lg:p-8 xl:p-10 flex rounded-[10px] justify-center p-2 bg-greyIsh relative bg-opacity-40">
-            <div className='searchBarContainer p-2 sm:p-4 md:p-6 lg:p-8 xl:p-10 flex flex-col justify-between items-center rounded-[8px] gap-[10px] bg-backgroundColor shadow-2xl w-[70%] min-w-[200px] '>
+            <div className='searchBarContainer p-2 sm:p-2 md:p-4 lg:p-6 xl:p-8 flex flex-col justify-between items-center rounded-[8px] gap-[10px] bg-backgroundColor shadow-2xl w-[70%] min-w-[200px] '>
                 <div className="flex gap-4 items-center w-[100%] relative">
                     <i className="pi pi-search sm:text-base md:text-lg lg:text-xl xl:text-2xl icon text-white" />
                     <div className='inputWrapper flex flex-col w-full relative'>
@@ -61,11 +61,12 @@ const SHSearchBar = () => {
                                 className="absolute right-0 top-1/2 transform -translate-y-1/2 px-2 text-gray-500 focus:outline-none"
                                 onClick={clearInput}
                             >
-                                Clear
+                                <i className="pi pi-times sm:text-base md:text-lg lg:text-xl xl:text-2xl icon text-white" />
+
                             </button>
                         )}
                         {showRecentSearches && (
-                            <div className="absolute left-0 w-full top-[30px] bg-white bg-opacity-80 rounded-b-md">
+                            <div className="absolute left-0 w-full top-[30px] bg-white bg-opacity-80 rounded-b-md z-[100]">
                                 <ul className="rounded shadow-lg mt-1">
                                     {filteredRecentSearches.map((search, index) => (
                                         <li
