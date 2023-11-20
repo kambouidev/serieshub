a0516fe4074c30d9767490634dfd7670c4ed1458
kambouidev <mohamed@squaads.com>
2023-11-20 20:14:26 +0000

    feat: implement card component

---
+++ src/components/SHCard/SHCard.component.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react'
+import { FaTrophy } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+type SHCardInfo = {
+    id: number;
+    image: string;
+    name: string;
+    link?: string;
+    premiereDate?: string;
+    episodes?: number;
+}
+
+type SHCardProps = {
+    data: SHCardInfo
+}
+const SHCard: FC<SHCardProps> = ({ data }) => {
+    const { id,
+        image,
+        name,
+        premiereDate,
+        link,
+        episodes } = data
+    return (
+        <div className="flex flex-row gap-2 mb-2 bg-white bg-opacity-10 rounded-lg" key={id}>
+            <div className="w-[150px] flex flex-col justify-center items-center">
+                {<img src={image} alt={name} className="rounded-md" />}
+            </div>
+            <div className="w-full flex flex-row flex-wrap justify-evenly text-white items-center">
+                <div className="justify-center items-center">
+                    <div className="font-extrabold text-base sm:text-xl lg:text-3xl mb-2 text-center">{name}</div>
+                    <div className="rating mb-1 text-xs sm:text-base lg:text-2xl flex flex-row items-center w-full justify-center">
+                        {premiereDate && <span className="flex flex-row">{premiereDate}<FaTrophy className="ml-1 text-amber-500" /></span>}
+                    </div>
+                </div>
+                {link && <div className="text-xs sm:text-lg mx-3">
+                    <Link to={`${link} `} >
+                        <button className="bg-blueColor p-2 rounded-lg">
+                            Episodies {episodes}
+                        </button>
+                    </Link>
+                </div>}
+            </div>
+        </div>
+    )
+}
+
+export default SHCard
--- src/pages/Show/ShowPage.tsx
+++ src/pages/Show/ShowPage.tsx
@@ -1,10 +1,11 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useShowSeasons } from "./hooks/useShowSeasons";
 import { useEffect } from "react";
 import { IShowWithEmbeddedSeasons } from "../../interfaces/SeasonInterface";
 import { SHHeader } from "../../components/SHHeader/SHHeader.component";
 import { FaStar, FaTrophy } from "react-icons/fa";
 import no_image from '../../assets/image_not_found.png';
+import SHCard from "../../components/SHCard/SHCard.component";
 const ShowPage = () => {
     const { data, isFetching, onSearchShow } = useShowSeasons();
     const { id } = useParams();
@@ -22,10 +23,18 @@ const ShowPage = () => {
     if (!data) return <><div>cargando</div></>;
 
     const { image, name, premiered, rating, _embedded, genres } = data as IShowWithEmbeddedSeasons;
-    const seasonsData = _embedded.seasons;
-    console.log(seasonsData)
+    const seasonsData = _embedded.seasons.map(({ id, image, name, number, premiereDate, episodeOrder }) => (
+        {
+            id: id,
+            image: image && image.original ? image.original : no_image,
+            name: `Season ${number} ${name.length > 0 ? " - " + name : ""}`,
+            link: `/seasons/${id}/episodes `,
+            premiereDate: `${new Date(premiereDate).getFullYear()}`,
+            episodes: episodeOrder
+        }));
+
     return (
-        <div className='w-[85%] m-auto '>
+        <div className='w-[85%] max-w-[1080px] m-auto '>
             <SHHeader />
             {isFetching && <div>Loading data</div>}
             {data &&
@@ -38,7 +47,7 @@ const ShowPage = () => {
                             </div>
                             <div className="w-1/2 flex flex-col justify-center gap-3 text-white items-center">
                                 <div className="title font-extrabold text-base mb-2">{name}</div>
-                                <div className="rating mb-1 text-xs flex flex-row items-center w-full justify-around">
+                                <div className="rating mb-1 text-xs flex flex-row items-center w-full justify-evenly">
                                     {premiered && <span className="flex flex-row">{new Date(premiered).getFullYear()}<FaTrophy className="ml-1 text-amber-500" /></span>}
                                     {rating && rating.average && <span className="flex flex-row">{rating.average}<FaStar className="ml-1 text-amber-500" /></span>}
                                 </div>
@@ -48,29 +57,7 @@ const ShowPage = () => {
                     </div>
                     <div>
                         <div className='p-10 text-center text-greyIsh opacity-40 text-lg sm:text-lg md:text-xl lg:text-3xl '>Seasons</div>
-                        {seasonsData.map((season) => (
-                            <div className="flex flex-row gap-2 mb-2 bg-white bg-opacity-10 rounded-lg" key={season.id}>
-                                <div className="w-[150px] flex flex-col justify-center items-center">
-                                    {<img src={season.image ? season.image.original || season.image.medium : no_image} alt={name} className="rounded-md" />}
-                                </div>
-                                <div className="w-full flex flex-row justify-center gap-1 text-white items-center">
-                                    <div className="w-1/2 justify-center items-center">
-                                        <div className="font-extrabold text-base mb-2 text-center">{`Season ${season.number}`}{season.name.length > 0 && ' - ' + season.name}</div>
-                                        <div className="rating mb-1 text-xs flex flex-row items-center w-full justify-center">
-                                            {season.premiereDate && <span className="flex flex-row">{new Date(season.premiereDate).getFullYear()}<FaTrophy className="ml-1 text-amber-500" /></span>}
-                                        </div>
-                                    </div>
-                                    <div className="w-1/2 p-2 text-sm text-center">
-                                        <Link to={`/seasons/${season.id}/episodes `} style={{ textDecoration: "none", color: "white" }}>
-                                            <button className="bg-blueColor p-2 rounded-lg">
-                                                Episodies {season.episodeOrder}
-
-                                            </button>
-                                        </Link>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
+                        {seasonsData.map((season) => <SHCard data={season} />)}
 
                     </div>
                 </div>
